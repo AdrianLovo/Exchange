66bf676ff182e4443f581cd993a7b1d0ae959d1f
AdrianLovo <adrianhumbertocastillo@gmail.com>
2020-12-11 00:43:29 -0600

    Primer commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
 
+import router from "@/router";
+import {dollarFilter, percentFilter} from '@/filters';
+
+Vue.filter('dollar', dollarFilter);
+Vue.filter('percent', percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
+  router: router,     //Utilizar el router
   render: h => h(App)
 }).$mount("#app");
---
+++ src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/views/Home';
+import About from '@/views/About';
+import Error from '@/views/Error';
+
+//Utilizar el Router
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',     //Utilizar el history mode de html
+
+    //Array de rutas
+    routes:[
+        {
+            path: '/',          //Ruta
+            name: 'home',       //Nombre
+            component: Home     //Componente
+        },
+        {
+            path: '/about',          //Ruta
+            name: 'about',       //Nombre
+            component: About     //Componente
+        },
+        {
+            path: '*',          //Ruta
+            name: 'Error',       //Nombre
+            component: Error     //Componente
+        }
+
+    ]
+})
